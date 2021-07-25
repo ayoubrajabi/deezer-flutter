31ac524b06b14097e29285c7bc61e9ba9ba9ebfd
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-07-25 22:23:26 +0430

    create project and init git..

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DeezerApp());
+}
+
+class DeezerApp extends StatelessWidget {
+  const DeezerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
