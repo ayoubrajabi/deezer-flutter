b2fe507b5272c972301f5d6448bc846378d93390
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-07-26 21:19:50 +0430

    init app theme.

---
+++ lib/view/config/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData? theme() {
+    return ThemeData(
+      scaffoldBackgroundColor: const Color(0xFF2C1B2A),
+      accentColor: const Color(0xFFA15A66),
+      cardColor: const Color(0xFF211B2D),
+    );
+  }
+}
