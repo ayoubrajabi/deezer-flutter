94f41be428714d6b8b2154d3023f693a10f1f53f
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-07-26 21:48:12 +0430

    add svg icons and icon asset class.

---
+++ lib/constants/icons_asset.dart
@@ -0,0 +1,7 @@
+class IconsAsset {
+  static const String _path = 'assets/svgs';
+
+  static const String home = '$_path/Home.svg';
+  static const String calendar = '$_path/Search.svg';
+  static const String activity = '$_path/arrowRight.svg';
+}
