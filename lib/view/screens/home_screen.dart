6eb66cebd68e7cf5ce95614f059aad59292de72f
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-07-29 13:36:31 +0430

    add loading widget.

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -75,9 +75,16 @@ class ForYouWidget extends StatelessWidget {
         },
         builder: (context, radioState) {
           if (radioState is RadioIsLoading) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
+            return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) => LoadingWidget(
+                      borderRadius: BorderRadius.circular(12.0),
+                      height: 300,
+                      width: 330.0,
+                      icon: Icons.settings_input_antenna,
+                      shape: BoxShape.rectangle,
+                    ));
           } else if (radioState is RadioIsLoaded) {
             return ListView.builder(
               itemCount: 10,
@@ -130,12 +137,15 @@ class ForYouWidget extends StatelessWidget {
                           ),
                         ),
                         child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 30.0, sigmaY: 30.0),
+                          filter: ImageFilter.blur(sigmaX: 40.0, sigmaY: 40.0),
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(radioState.getRadio.data![index].title!),
                               Text(
                                   'tracks: ${radioState.getRadio.data![index].nbTracks}'),
+                              Text(
+                                  'tracks: ${radioState.getRadio.data![index].user!.name!}'),
                             ],
                           ),
                         ),
@@ -174,9 +184,15 @@ class TopArtistsWidget extends StatelessWidget {
         },
         builder: (context, radioState) {
           if (radioState is ArtistIsLoading) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
+            return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) => LoadingWidget(
+                      // height: 300,
+                      width: 130.0,
+                      icon: Icons.person,
+                      shape: BoxShape.circle,
+                    ));
           } else if (radioState is ArtistIsLoaded) {
             return ListView.builder(
               itemCount: 10,
@@ -210,6 +226,49 @@ class TopArtistsWidget extends StatelessWidget {
           return const SizedBox();
         },
       ),
+    );
+  }
+}
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({
+    Key? key,
+    this.height,
+    this.width,
+    this.shape,
+    this.icon,
+    this.borderRadius,
+  }) : super(key: key);
+  final double? height;
+  final double? width;
+  final IconData? icon;
+  final BoxShape? shape;
+  final BorderRadius? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
+    return Container(
+      height: height,
+      width: width,
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: _theme.cardColor,
+        shape: shape!,
+        borderRadius: borderRadius,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10.0,
+          ),
+        ],
+      ),
+      child: Center(
+          child: Icon(
+        icon,
+        color: Colors.grey,
+        size: 70.0,
+      )),
     );
   }
 }
