302fc822f77628a374914afb5164e4efcacf1bf2
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-07-31 21:43:38 +0430

    add radiobloc to view more screen.

--- lib/view/screens/view_more_screen.dart
+++ lib/view/screens/view_more_screen.dart
@@ -1,14 +1,33 @@
+import 'package:deezer_flutter/logic/logics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ViewMoreScreen extends StatelessWidget {
   const ViewMoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.white,
-      ),
+    return Builder(
+      builder: (context) {
+        final _radioState = context.watch<RadioBloc>().state;
+
+        if (_radioState is RadioIsLoading) {
+          return Container(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (_radioState is RadioIsLoaded) {
+          return Column(
+            children: [
+              AppBar(
+                title: Text(_radioState.getRadio.data![2].title!),
+              ),
+            ],
+          );
+        }
+        return const SizedBox();
+      },
     );
   }
 }
