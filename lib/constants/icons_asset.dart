3eb3828fdb38e85cab5e85b3ec0a9bc064554a77
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-08-08 14:35:01 +0430

    add search text field and deezer icon in search_screen.

--- lib/constants/icons_asset.dart
+++ lib/constants/icons_asset.dart
@@ -6,4 +6,5 @@ class IconsAsset {
   static const String arrow = '$_path/Arrow.svg';
   static const String arrow2 = '$_path/Arrow 2.svg';
   static const String deezer = '$_path/deezer.svg';
+  static const String deezerIcon = '$_path/Deezer_Icon.svg';
 }
