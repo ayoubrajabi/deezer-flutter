22b81e7224e496d6300c3f8e50fb7118378a0ffa
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-08-09 20:32:36 +0430

    add top_artist and music_item in search screen.

--- lib/view/screens/search_screen.dart
+++ lib/view/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:deezer_flutter/logic/logics.dart';
+import 'package:deezer_flutter/view/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -54,6 +55,10 @@ class SearchScreen extends StatelessWidget {
                           {'q': value},
                         ),
                       ),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14.0,
+                  ),
                   decoration: InputDecoration(
                     fillColor: Colors.white10,
                     hintText: 'Search Artists, Albums, Tracks ...',
@@ -81,8 +86,51 @@ class SearchScreen extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
-            child: const SizedBox(
+            child: SizedBox(
               height: 1000.0,
+              child: BlocBuilder<SearchBloc, SearchState>(
+                builder: (context, searchState) {
+                  if (searchState is SearchIsLoading) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (searchState is SearchIsLoaded) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TopArtistItem(
+                          onTap: () {},
+                          imgUrl: searchState
+                              .getRadio.data![0].artist!.pictureMedium,
+                          artistName:
+                              searchState.getRadio.data![0].artist!.name,
+                        ),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        SizedBox(
+                          height: 400.0,
+                          child: ListView.builder(
+                            itemCount: 3,
+                            itemBuilder: (context, index) {
+                              return HotMusicItems(
+                                artistName: searchState
+                                    .getRadio.data![index].artist!.name,
+                                imgUrl: searchState
+                                    .getRadio.data![index].album!.coverMedium,
+                                musicTitle:
+                                    searchState.getRadio.data![index].title,
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+
+                  return const SizedBox();
+                },
+              ),
             ),
           ),
         ],
